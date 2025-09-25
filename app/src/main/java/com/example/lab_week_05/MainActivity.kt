7b99520fa039090a5d6b0d987e7dc63f7820f8de
calvin <jf.calvin20@gmail.com>
2025-09-25 13:46:55 +0700

    Commit No. 2 - Parse JSON with Moshi and Display Image URL

--- app/src/main/java/com/example/lab_week_05/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_05/MainActivity.kt
@@ -10,12 +10,14 @@ import com.example.lab_week_05.api.CatApiService
 import retrofit2.*
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import android.util.Log
+import com.example.lab_week_05.model.ImageData
+import retrofit2.converter.moshi.MoshiConverterFactory
 
 class MainActivity : AppCompatActivity() {
     private val retrofit by lazy {
         Retrofit.Builder()
             .baseUrl("https://api.thecatapi.com/v1/")   // base URL dari Cat API
-            .addConverterFactory(ScalarsConverterFactory.create()) // converter biar response bisa jadi String
+            .addConverterFactory(MoshiConverterFactory.create()) // converter biar response bisa jadi String
             .build()
     }
 
@@ -36,14 +38,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun getCatImageResponse() {
         val call = catApiService.searchImages(1, "full")
-        call.enqueue(object : Callback<String> {
-            override fun onFailure(call: Call<String?>, t: Throwable) {
+        call.enqueue(object : Callback<List<ImageData>> {
+            override fun onFailure(call: Call<List<ImageData>>, t: Throwable) {
                 Log.e(MAIN_ACTIVITY, "Failed to get response", t)
             }
 
-            override fun onResponse(call: Call<String?>, response: Response<String>) {
+            override fun onResponse(call: Call<List<ImageData>>, response: Response<List<ImageData>>) {
                 if (response.isSuccessful) {
-                    apiResponseView.text = response.body()
+                   val image = response.body()
+                    val firstImage = image?.firstOrNull()?.imageUrl ?: "No URL"
+                    apiResponseView.text = getString(R.string.image_placeholder, firstImage)
                 } else {
                     Log.e(
                         MAIN_ACTIVITY,
